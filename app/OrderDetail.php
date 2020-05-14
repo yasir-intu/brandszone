c9233c3af22eb857d8845fc722357118ec95c86b
Yasir <yasirintisar7@gmail.com>
2020-05-14 13:13:24 +0600

    supplier work is goin on

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    public function products()
+    {
+        return $this->hasOne('App\Product', 'id', 'product_id');
+    }
 }
