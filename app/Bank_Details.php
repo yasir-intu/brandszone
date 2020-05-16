f9173b97b93235c7f9de4a49c559c3463f9fdefb
Yasir <yasirintisar7@gmail.com>
2020-05-17 01:00:13 +0600

    supplier bank work going

--- app/Bank_Details.php
+++ app/Bank_Details.php
@@ -7,11 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Bank_Details extends Model
 {
     public function bank(){
-        return $this->belongsTo('App\Bank', 'id', 'bank_id');
+        return $this->belongsTo('App\Bank', 'bank_id', 'id');
     }
-
+    
     public function branch(){
-        return $this->belongsTo('App\Bank', 'id', 'branch_id');
+        return $this->belongsTo('App\Bank', 'branch_id', 'id');
     }
-
+    
+    public function country(){
+        return $this->belongsTo('App\Country', 'country_id', 'id');
+    }
 }
