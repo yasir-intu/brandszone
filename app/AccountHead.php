5eb3f26e2df3475c670bd21a1fe0faec5da96c29
Yasir <yasirintisar7@gmail.com>
2020-06-07 12:20:56 +0600

    supplier

--- app/AccountHead.php
+++ app/AccountHead.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccountHead extends Model
 {
-    //
+    public function account(){
+        return $this->hasMany('App\Account', 'head_id', 'id');
+    }
 }
