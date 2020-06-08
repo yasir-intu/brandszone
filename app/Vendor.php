6dc46560adc8aaee33951f77f2aaf738523953fd
Yasir <yasirintisar7@gmail.com>
2020-06-08 13:14:39 +0600

    supplier

--- app/Vendor.php
+++ app/Vendor.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
-    //
+    public function bank(){
+        return $this->belongsTo('App\Bank', 'bank_id', 'id');
+    }
+    
+    public function branch(){
+        return $this->belongsTo('App\Bank_branch', 'branch_id', 'id');
+    }
+    
+    public function country(){
+        return $this->belongsTo('App\Country', 'country_id', 'id');
+    }
 }
