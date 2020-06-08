fd5f0085bac5410a6d2855e40ecc14b1a6d5d774
Yasir <yasirintisar7@gmail.com>
2020-06-08 13:14:24 +0600

    supplier

--- app/PaymentTransfer.php
+++ app/PaymentTransfer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class PaymentTransfer extends Model
 {
-    //
+    public function accountHead(){
+        return $this->belongsTo('App\AccountHead', 'head_id', 'id');
+    }
+
+    public function vendor(){
+        return $this->belongsTo('App\Vendor', 'account_id', 'id');
+    }
+
+    public function employee(){
+        return $this->belongsTo('App\Employee', 'account_id', 'id');
+    }
 }
