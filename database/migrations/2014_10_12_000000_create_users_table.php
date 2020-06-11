d053b5da39e858aee9ab2fb89716fa508d1e91b3
Yasir <yasirintisar7@gmail.com>
2020-06-11 18:14:45 +0600

    supplier

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('role_id');
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone');
